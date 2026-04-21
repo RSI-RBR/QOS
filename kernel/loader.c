613d5b5f16b5098f2a495d1d0aa9c84f7f36ac72
Republic Security and Intelligence <rsi00rbr@gmail.com>
2026-04-21 12:21:35 -0400

    Update loader.c

--- kernel/loader.c
+++ kernel/loader.c
@@ -9,44 +9,19 @@
 #define PROGRAM_SECTOR 100
 #define PROGRAM_SECTORS 64 // size 32KB
 
-static unsigned char program_buffer[PROGRAM_MAX_SIZE];
-
-void *load_program_from_sd(const char *path){
-    uart_puts("Loading program from SD... \n");
-
-    int size = sd_read_file(path, program_buffer, PROGRAM_MAX_SIZE);
-
-    if (size <= 0){
-        uart_puts("Failed to load program\n");
-        return 0;
-    }
-
-    uart_puts("Program loaded, size: ");
-    uart_puthex(size);
-    uart_puts("\n");
+void *load_program_from_sd(void)
+{
+    uart_puts("Loading program from SD...\n");
 
     unsigned char *dst = (unsigned char*)PROGRAM_ADDR;
 
-    for (int i = 0; i < size; i++){
-        dst[i] = program_buffer[i];
+    // read directly into execution memory
+    if (sd_read_blocks(PROGRAM_SECTOR, dst, PROGRAM_SECTORS) != 0) {
+        uart_puts("SD read failed!\n");
+        return 0;
     }
 
-    uart_puts("Program copied to exec memory\n");
+    uart_puts("Program loaded into memory\n");
 
     return (void*)PROGRAM_ADDR;
-}
-
-//extern unsigned char _binary_build_program_bin_start[];
-//extern unsigned char _binary_build_program_bin_end[];
-
-void* load_program(const void *src, unsigned long size, void *dst){
-    uart_puts("Loading program...\n");
-
-    for (unsigned long i = 0; i < size; i++){
-        ((unsigned char*)dst)[i] = ((unsigned char*)src)[i];
-    }
-
-    uart_puts("Program loaded\n");
-
-    return 0;
 }
