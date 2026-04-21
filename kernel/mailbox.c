a9bd4badda70dcd281563a5c7026eeaf326e6964
Republic Security and Intelligence <rsi00rbr@gmail.com>
2026-04-21 08:45:39 -0400

    Update mailbox.c

--- kernel/mailbox.c
+++ kernel/mailbox.c
@@ -1,16 +1,16 @@
 #include "mailbox.h"
 
-#define MMIO_BASE       0x3F000000
-#define MBOX_BASE       (MMIO_BASE + 0xB880)
+#define MMIO_BASE 0x3F000000
+#define MBOX_BASE (MMIO_BASE + 0xB880)
 
-#define MBOX_READ       (*(volatile unsigned int*)(MBOX_BASE + 0x0))
-#define MBOX_STATUS     (*(volatile unsigned int*)(MBOX_BASE + 0x18))
-#define MBOX_WRITE      (*(volatile unsigned int*)(MBOX_BASE + 0x20))
+#define MBOX_READ   (*(volatile unsigned int*)(MBOX_BASE + 0x0))
+#define MBOX_STATUS (*(volatile unsigned int*)(MBOX_BASE + 0x18))
+#define MBOX_WRITE  (*(volatile unsigned int*)(MBOX_BASE + 0x20))
 
-#define MBOX_EMPTY      0x40000000
-#define MBOX_FULL       0x80000000
+#define MBOX_EMPTY 0x40000000
+#define MBOX_FULL  0x80000000
 
-static volatile unsigned int mbox[36] __attribute__((aligned(16)));
+volatile unsigned int mbox[36] __attribute__((aligned(16)));
 
 int mailbox_call(unsigned char ch){
     unsigned int r = ((unsigned int)((unsigned long)&mbox) & ~0xF) | (ch & 0xF);
