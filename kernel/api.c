97c6e1097880d5051b382431fdc08bc78339331b
Republic Security and Intelligence <rsi00rbr@gmail.com>
2026-04-21 18:35:48 -0400

    Update api.c

--- kernel/api.c
+++ kernel/api.c
@@ -1,7 +1,10 @@
 #include "api.h"
 #include "uart.h"
+#include "memory.h"
 
 kernel_api_t kapi = {
     .puts = uart_puts,
-    .putc = uart_send
+    .putc = uart_send,
+    .malloc = kmalloc,
+    .free = kfree
 };
