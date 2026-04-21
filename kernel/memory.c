0b17c7ebee3c8de6d4ed58437335c502693b5d37
Republic Security and Intelligence <rsi00rbr@gmail.com>
2026-04-21 18:33:23 -0400

    Update memory.c

--- kernel/memory.c
+++ kernel/memory.c
@@ -1,53 +1,84 @@
 #include "memory.h"
+#include "uart.h"
 
-#define HEAP_SIZE 1024*1024 // 1MB heap
+#define HEAP_SIZE (16 * 1024 * 1024) // 16MB for now (expand later)
 
 static unsigned char heap[HEAP_SIZE];
-static unsigned long heap_top = 0;
 
-typedef struct {
-    void *ptr;
+typedef struct block {
     unsigned long size;
-    int used;
-} alloc_t;
+    int free;
+    struct block *next;
+} block_t;
 
-#define MAX_ALLOCS 32
-static alloc_t allocs[MAX_ALLOCS];
+static block_t *free_list = 0;
+
+#define ALIGN16(x) (((x) + 15) & ~15)
 
 void memory_init(void){
-    heap_top = 0;
+    free_list = (block_t*)heap;
 
-    for (int i = 0; i < MAX_ALLOCS; i++){
-        allocs[i].used = 0;
-    }
+    free_list->size = HEAP_SIZE - sizeof(block_t);
+    free_list->free = 1;
+    free_list->next = 0;
+
+    uart_puts("Heap initialized\n");
 }
 
-void *kmalloc(unsigned int size){
-    if (heap_top + size >= HEAP_SIZE){
-        return 0;
+static void split_block(block_t *block, unsigned long size){
+    block_t *new_block = (block_t*)((unsigned char*)block + sizeof(block_t) + size);
+
+    new_block->size = block->size - size - sizeof(block_t);
+    new_block->free = 1;
+    new_block->next = block->next;
+
+    block->size = size;
+    block->next = new_block;
+}
+
+void *kmalloc(unsigned long size){
+    if (size == 0) return 0;
+
+    size = ALIGN16(size);
+
+    block_t *curr = free_list;
+
+    while (curr){
+        if (curr->free && curr->size >= size){
+
+            if (curr->size > size + sizeof(block_t)){
+                split_block(curr, size);
+            }
+
+            curr->free = 0;
+            return (void*)((unsigned char*)curr + sizeof(block_t));
+        }
+
+        curr = curr->next;
     }
 
-    void *ptr = &heap[heap_top];
-    heap_top += size;
+    uart_puts("kmalloc failed!\n");
+    return 0;
+}
 
-    for (int i = 0; i < MAX_ALLOCS; i++){
-        if (!allocs[i].used){
-            allocs[i].ptr = ptr;
-            allocs[i].size = size;
-            allocs[i].used = 1;
-            break;
+static void merge_blocks(){
+    block_t *curr = free_list;
+
+    while (curr && curr->next){
+        if (curr->free && curr->next->free){
+            curr->size += sizeof(block_t) + curr->next->size;
+            curr->next = curr->next->next;
+        } else {
+            curr = curr->next;
         }
     }
-
-    return ptr;
-
 }
 
 void kfree(void *ptr){
-    for (int i = 0; i < MAX_ALLOCS; i++){
-        if (allocs[i].used && allocs[i].ptr == ptr){
-            allocs[i].used = 0;
-            return;
-        }
-    }
+    if (!ptr) return;
+
+    block_t *block = (block_t*)((unsigned char*)ptr - sizeof(block_t));
+    block->free = 1;
+
+    merge_blocks();
 }
