dbb3486fd76168f24dd165e56f0fab588bbbef2f
Republic Security and Intelligence <rsi00rbr@gmail.com>
2026-04-22 03:34:02 -0400

    Refactor sd.h to include only necessary function prototypes
    Removed unused function declarations and included necessary function prototypes for SD card operations.

--- include/sd.h
+++ include/sd.h
@@ -1,14 +1,7 @@
 #ifndef SD_H
 #define SD_H
 
-#include <stdint.h>
-
-void sd_gpio_init(void);
-int sd_init();
-int sd_read_block(uint32_t lba, uint8_t *buffer);
-
-int sd_read_file(const char *path, void *buf, int max_size);
-
-int sd_read_blocks(uint32_t lba, uint8_t *buffer, int count);
+int sd_init(void);
+int sd_read_block(unsigned int lba, unsigned char *buffer);
 
 #endif
