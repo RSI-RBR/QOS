f86561d2f24364f995c81a80d0b40f4e3b6f4658
RSI-RBR <rsi00rbr@gmail.com>
2026-04-28 20:10:21 -0400

    Editing test program (to be screen saver)

--- programs/hello/main.c
+++ programs/hello/main.c
@@ -1,16 +1,55 @@
 #include "api.h"
 
+struct screen_saver_cube{
+    int lx;
+    int ly;
+    int sx;
+    int sy;
+    int vx;
+    int vy;
+    unsigned int c;
+};
+
 void program_main(kernel_api_t *api){
     api->puts("Hello from external program!\n");
 
-    api->draw_rect(200, 200, 200, 100, 0x00FF0000);
+    int screen_x = 1920; int screen_y = 1080;
+//    api->draw_rect(0, 0, 50, 50, 0x00FF0000);
+    char running = 1;
+    struct screen_saver_cube cube; cube.lx = 0; cube.ly = 0; cube.vx = 1; cube.vy = 1; cube.c = 0x00FFFFFF; cube.sx = 50; cube.sy = 50;
+    while (running){
+        api->clear(0x00000000);
+        int nx = cube.lx + cube.vx;
+        int ny = cube.ly + cube.vy;
 
-    void *mem = api->malloc(256);
+        if (nx < 0){
+            nx = 0;
+            cube.vx *= -1;
+        }else if (nx + cube.sx > screen_x){
+            nx = screen_x;
+            cube.vx *= -1;
+        }
 
-    if (mem){
-        api->puts("Allocated memory\n");
-        api->free(mem);
+        if (ny < 0){
+            ny = 0;
+            cube.vy *= -1;
+        } else if (ny + cube.sy > screen_y){
+            ny = screen_y;
+            cube.vy *= -1;
+        }
+
+        cube.lx = nx; cube.ly = ny;
+
+        api->draw_rect(cube.lx, cube.ly, cube.sx, cube.sy, cube.c);
     }
-    api->puts("Program main returning...\n");
+    
+
+//    void *mem = api->malloc(256);
+//
+//    if (mem){
+//        api->puts("Allocated memory\n");
+//        api->free(mem);
+//    }
+//    api->puts("Program main returning...\n");
     return;
 }
