1cb9dbca100f591b37af5d9c3047b60d8da5a430
RSI-RBR <rsi00rbr@gmail.com>
2026-04-30 16:22:15 -0400

    Fix mailbox cache coherency for framebuffer setup

--- kernel/mailbox.c
+++ kernel/mailbox.c
@@ -15,8 +15,31 @@
 
 volatile unsigned int mbox[36] __attribute__((aligned(16)));
 
+static unsigned long cache_line_size(void){
+    unsigned long ctr;
+    asm volatile("mrs %0, ctr_el0" : "=r"(ctr));
+    // DminLine is log2(words). bytes = 4 << DminLine.
+    return 4UL << ((ctr >> 16) & 0xF);
+}
+
+static void clean_invalidate_dcache_range(unsigned long start, unsigned long size){
+    unsigned long line = cache_line_size();
+    unsigned long addr = start & ~(line - 1);
+    unsigned long end = (start + size + line - 1) & ~(line - 1);
+
+    for (; addr < end; addr += line){
+        asm volatile("dc civac, %0" : : "r"(addr) : "memory");
+    }
+    asm volatile("dsb ish");
+}
+
 int mailbox_call(unsigned char ch){
     unsigned int r = ((unsigned int)((unsigned long)&mbox) & ~0xF) | (ch & 0xF);
+    unsigned long mbox_addr = (unsigned long)&mbox[0];
+    unsigned long mbox_size = sizeof(mbox);
+
+    // Make request visible to GPU before ringing mailbox doorbell.
+    clean_invalidate_dcache_range(mbox_addr, mbox_size);
 
     while (MBOX_STATUS & MBOX_FULL);
     MBOX_WRITE = r;
@@ -24,8 +47,11 @@ int mailbox_call(unsigned char ch){
     while (1){
         while (MBOX_STATUS & MBOX_EMPTY);
 
-        if (MBOX_READ == r)
+        if (MBOX_READ == r){
+            // Refresh CPU view of response written by GPU.
+            clean_invalidate_dcache_range(mbox_addr, mbox_size);
             return mbox[1] == 0x80000000;
+        }
     }
 }
 
