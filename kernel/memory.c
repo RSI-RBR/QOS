2bad509f1eb9b3902d457f71b714b097142bc245
RSI-RBR <rsi00rbr@gmail.com>
2026-05-02 16:52:54 -0400

    Update commit message formatting

--- kernel/memory.c
+++ kernel/memory.c
@@ -1,14 +1,10 @@
 #include "memory.h"
 #include "uart.h"
-#include "mailbox.h"
 
+#define HEAP_SIZE (16 * 1024 * 1024) // 16MB stable baseline
 #define BLOCK_MAGIC 0xB10CB10CUL
-#define HEAP_FALLBACK_BASE 0x00000000UL
-#define HEAP_FALLBACK_SIZE (1024UL * 1024UL * 1024UL) // 1GB fallback
-#define HEAP_PHYS_CEIL 0x3F000000UL // keep below peripheral MMIO window
-#define HEAP_MIN_BYTES (64UL * 1024UL)
 
-extern unsigned long core3_stack_top;
+static unsigned char heap[HEAP_SIZE];
 
 typedef struct block {
     unsigned long magic;
@@ -18,52 +14,18 @@ typedef struct block {
 } block_t;
 
 static block_t *free_list = 0;
-static unsigned char *heap_base = 0;
-static unsigned long heap_size = 0;
 
 #define ALIGN16(x) (((x) + 15) & ~15)
 
 void memory_init(void){
-    unsigned int arm_base32 = 0;
-    unsigned int arm_size32 = 0;
-    unsigned long arm_base = HEAP_FALLBACK_BASE;
-    unsigned long arm_size = HEAP_FALLBACK_SIZE;
-    if (mailbox_get_arm_memory(&arm_base32, &arm_size32) == 0){
-        arm_base = (unsigned long)arm_base32;
-        arm_size = (unsigned long)arm_size32;
-    }
-
-    unsigned long heap_start = ALIGN16((unsigned long)&core3_stack_top + 0x1000UL);
-    unsigned long heap_end = arm_base + arm_size;
-    if (heap_end > HEAP_PHYS_CEIL){
-        heap_end = HEAP_PHYS_CEIL;
-    }
-    if (heap_start < arm_base){
-        heap_start = ALIGN16(arm_base);
-    }
-    if (heap_end <= heap_start || (heap_end - heap_start) < (sizeof(block_t) + HEAP_MIN_BYTES)){
-        uart_puts("Heap init failed: invalid heap range\n");
-        free_list = 0;
-        heap_base = 0;
-        heap_size = 0;
-        return;
-    }
-
-    heap_base = (unsigned char*)heap_start;
-    heap_size = heap_end - heap_start;
-    free_list = (block_t*)heap_base;
+    free_list = (block_t*)heap;
 
     free_list->magic = BLOCK_MAGIC;
-    free_list->size = heap_size - sizeof(block_t);
+    free_list->size = HEAP_SIZE - sizeof(block_t);
     free_list->free = 1;
     free_list->next = 0;
 
     uart_puts("Heap initialized\n");
-    uart_puts("Heap base=");
-    uart_puthex((unsigned int)(unsigned long)heap_base);
-    uart_puts(" size=");
-    uart_putdec(heap_size / (1024UL * 1024UL));
-    uart_puts("MB\n");
 }
 
 static void split_block(block_t *block, unsigned long size){
@@ -79,7 +41,7 @@ static void split_block(block_t *block, unsigned long size){
 }
 
 void *kmalloc(unsigned long size){
-    if (size == 0 || !free_list){
+    if (size == 0){
         return 0;
     }
 
@@ -89,7 +51,6 @@ void *kmalloc(unsigned long size){
 
     while (curr){
         if (curr->free && curr->size >= size){
-
             if (curr->size > size + sizeof(block_t)){
                 split_block(curr, size);
             }
@@ -120,7 +81,7 @@ static void merge_blocks(){
 
 static int ptr_in_heap(void *ptr){
     unsigned char *p = (unsigned char*)ptr;
-    return heap_base && p >= (heap_base + sizeof(block_t)) && p < (heap_base + heap_size);
+    return p >= (heap + sizeof(block_t)) && p < (heap + HEAP_SIZE);
 }
 
 static block_t* ptr_to_block(void *ptr){
@@ -135,7 +96,9 @@ static block_t* ptr_to_block(void *ptr){
 }
 
 void kfree(void *ptr){
-    if (!ptr) return;
+    if (!ptr){
+        return;
+    }
 
     block_t *block = ptr_to_block(ptr);
     if (!block){
@@ -176,5 +139,4 @@ void kfree_secure(void* ptr, unsigned long size){
     }
 
     kfree(ptr);
-
 }
