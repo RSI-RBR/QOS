ce01b8b188dfd9209469c57c14a6f5581911fc3b
RSI-RBR <rsi00rbr@gmail.com>
2026-05-07 18:57:10 -0400

    Add standard memory prototypes for PQClean

--- include/string.h
+++ include/string.h
@@ -1,8 +1,15 @@
 #ifndef STRING_H
 #define STRING_H
 
+#include <stddef.h>
+
 int kstrlen(const char *str);
 int kstrcmp(const char *a, const char *b);
 void kstrcpy(char *dest, const char *src);
+
+void *memcpy(void *dest, const void *src, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
+void *memset(void *dest, int c, size_t n);
+int memcmp(const void *a, const void *b, size_t n);
 
 #endif
