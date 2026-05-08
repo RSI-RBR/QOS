a7d27a4db1176bb58b144d95cdef9009eac3a22f
RSI-RBR <rsi00rbr@gmail.com>
2026-05-07 22:12:37 -0400

    Enable SMP coherency in EL2 boot path

--- include/cpu.h
+++ include/cpu.h
@@ -7,16 +7,4 @@ static inline unsigned int cpu_get_id(void){
     return (unsigned int)(mpidr & 0xFFUL);
 }
 
-static inline void cpu_enable_smp_coherency(void){
-    // Cortex-A53 requires CPUECTLR_EL1.SMPEN=1 for hardware data-cache
-    // coherency across cores. Some firmware enables this, but we set it
-    // explicitly to avoid long-run SMP instability.
-    unsigned long v;
-    asm volatile("mrs %0, s3_1_c15_c2_1" : "=r"(v));
-    v |= (1UL << 6); // SMPEN
-    asm volatile("msr s3_1_c15_c2_1, %0" : : "r"(v));
-    asm volatile("isb");
-    asm volatile("dmb ish");
-}
-
 #endif
